8c6173fc3161470a694e7ad075e7cbb334d81b4b
shrest21 <shrest21@gmail.com>
2026-01-04 13:51:32 +0530

    Added view my orders endpoint

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { StorageService } from '../_services/storage.service';
 
 @Component({
   selector: 'app-profile',
+  standalone:true,
+  imports:[CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-
+  constructor(private storageService:StorageService){}
+  user=this.storageService.getUser();
 }
