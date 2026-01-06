11b942cdb784bf34b6c723ffee40ba1fb8f5f486
shrest21 <shrest21@gmail.com>
2026-01-06 14:29:50 +0530

    Add feature to updatepayment and styled the form

--- src/app/updatepayment/updatepayment.component.ts
+++ src/app/updatepayment/updatepayment.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UpdatepaymentService } from '../_services/updatepayment.service';
+import { StorageService } from '../_services/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-updatepayment',
@@ -10,10 +12,18 @@ import { UpdatepaymentService } from '../_services/updatepayment.service';
   templateUrl: './updatepayment.component.html',
   styleUrl: './updatepayment.component.css'
 })
-export class UpdatepaymentComponent {
+export class UpdatepaymentComponent implements OnInit{
   invoiceId=0;
   status='';
-  constructor(private updatepaymentService:UpdatepaymentService){}
+  constructor(private updatepaymentService:UpdatepaymentService,private storageService:StorageService,private route:Router){}
+  ngOnInit(): void {
+    const user=this.storageService.getUser();
+    if(user.role!="ROLE_FINANCE_OFFICER")
+    {
+      alert('Please login as finance officer');
+      this.route.navigate(['/products']);
+    }
+  }
   update(){
     const payload={
       status:this.status
